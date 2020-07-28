63e06a5d1eeb0d3d5ddb1eccf5ec72ee31c5517e
tsultana5226 <tsultana5226@bths.edu>
2020-07-28 09:46:44 -0400

    working add event button

--- app.py
+++ app.py
@@ -45,30 +45,3 @@ def index():
         # add to the data base
         # events.insert({'username': username})
         return redirect('/')
-
-@app.route("/calendar",  methods = ['GET', 'POST'])
-def calendar():
-    if request.method == "POST":
-        title = request.form["title"]
-        #connect to a database
-        events = mongo.db.events
-        # add to the data base
-        events.insert({'title': title})
-    return render_template('calendar.html', time=datetime.now())
-
-@app.route('/input', methods = ['GET', 'POST'])
-def input():
-    if request.method == "POST":
-        username = request.form['username']
-        print(username)
-        #connect to a database
-        events = mongo.db.events
-        # add to the data base
-        events.insert({'username': username})
-    return render_template("input.html", time=datetime.now())
-
-@app.route("/form", methods=['POST'])
-def form():
-    return render_template("input.html", time=datetime.now())
-
-    
